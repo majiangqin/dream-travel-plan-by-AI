8a0ce115d131de43c90e821a0b98b3e3700a6875
Jiangqin Ma <jma416@gatech.edu>
2024-06-03 22:50:26 -0700

    add files

---
+++ genkit-functions/src/lib/genkit.config.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { configureGenkit } from '@genkit-ai/core';
+import { dotprompt } from '@genkit-ai/dotprompt';
+import { firebase } from '@genkit-ai/firebase';
+import { vertexAI } from '@genkit-ai/vertexai';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBT_DIkIH7OuAoG4lNheR0BE0svDNgMM5k",
+  authDomain: "kidbank-app.firebaseapp.com",
+  projectId: "kidbank-app",
+  storageBucket: "kidbank-app.appspot.com",
+  messagingSenderId: "890160777266",
+  appId: "1:890160777266:web:be31c518c2364b9d4f542b",
+  measurementId: "G-C351CR82SL"
+};
+
+
+export const getProjectId = () => {
+  return firebaseConfig.projectId;
+};
+
+export const config = configureGenkit({
+  plugins: [
+    firebase({
+      projectId: getProjectId(),
+      traceStore: { collection: 'traceStore' },
+      flowStateStore: { collection: 'flowTraceStore' },
+    }),
+    vertexAI({
+      projectId: getProjectId(),
+      location: 'us-central1',
+    }),
+    dotprompt(),
+  ],
+  flowStateStore: 'firebase',
+  traceStore: 'firebase',
+  enableTracingAndMetrics: true,
+  logLevel: 'debug',
+});
